d1849c9b85484a6ee13cd8957dc166150495a6dc
Vignesh Moorthy <madhanvic@gmail.com>
2024-04-25 04:37:38 +0530

    boilerplate react setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./index.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Movies from "./pages/Movies.tsx";
